352c37eba5dde60f5059a6bfee261a7d9579640c
ice <1499046615@qq.com>
2019-07-05 10:43:37 +0800

    test4

--- src/test/java/com/zs/git01/Git01ApplicationTests.java
+++ src/test/java/com/zs/git01/Git01ApplicationTests.java
@@ -5,14 +5,15 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
-public class Git01ApplicationTests {
+    @RunWith(SpringRunner.class)
+    @SpringBootTest
+    public class Git01ApplicationTests {
 
-    @Test
-    public void contextLoads() {
-        System.out.println("i am ok");
-        System.out.println("ok");
-    }
+        @Test
+        public void contextLoads() {
+            System.out.println("i am ok");
+            System.out.println("ok");
+            System.out.println("lol");
+        }
 
 }
